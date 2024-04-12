1d5675de82341c29abd65ce9f4707a6a0538ed49
Eighty Klisto <acecalisto3@users.noreply.huggingface.co>
2024-04-12 02:48:29 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+class Config:
+    API_KEY = "api_key"  # Placeholder for the API key, initially set to None
+
+    @classmethod
+    def set_api_key(cls, api_key):
+        cls.API_KEY = api_key
+
+    @classmethod
+    def get_api_key(cls):
+        return cls.API_KEY
