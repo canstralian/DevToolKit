0536d9321fb7dc772e43d4d9576c938c7b26e288
Eighty Klisto <acecalisto3@users.noreply.huggingface.co>
2024-04-12 23:31:52 +0000

    Update components/__init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,1 +1,9 @@
-from .home import code_documentation_page
+# components/__init__.py
+from .home import HomePage
+from .refactor_page import RefactorPage
+from .style_page import StylePage
+from .test_page import TestPage
+from .optimize_page import OptimizePage
+
+
+# from .documentation_page import DocumentationPage
