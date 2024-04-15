c7e464ec3c5407068a6e8f0da67f09eed122f9a7
Eighty Klisto <acecalisto3@users.noreply.huggingface.co>
2024-04-15 01:32:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import gradio as gr
 from transformers import AutoModel, AutoTokenizer
 
-   os.environ["GRADIO_SERVER_PORT"] = "8507"
+os.environ["GRADIO_SERVER_PORT"] = "8507"
 
 def get_code_generative_models():
     models_dir = os.path.join(os.getcwd(), "models")
