e028678518f11c3fe848bf4a7bad57acbca0930a
Eighty Klisto <acecalisto3@users.noreply.huggingface.co>
2024-04-15 01:57:16 +0000

    Delete components

--- components/__init__.py
+++
@@ -1,7 +0,0 @@
-# components/__init__.py
-from .home import HomePage
-from .refactor_page import RefactorPage
-from .style_page import StylePage
-from .test_page import TestPage
-from .optimize_page import OptimizePage
-from .documentation_page import DocumentationPage
