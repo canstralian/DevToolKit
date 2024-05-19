101e11f0ed4d1e3b89f095105d33b970ddf5203c
Eighty Klisto <acecalisto3@users.noreply.huggingface.co>
2024-05-19 19:04:48 +0000

    Rename gemmacode/tokenizer.py to tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,5 @@
 import transformers
 
 class CodeTokenizer(transformers.AutoTokenizer):
-    def __init__(self, model_name):
+    def __init__(self, PhiCo-D-Instruct):
         super().__init__.from_pretrained(model_name)
