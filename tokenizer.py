6f2142cf23017d996ab4c816eaa1cac463eca328
Eighty Klisto <acecalisto3@users.noreply.huggingface.co>
2024-05-28 08:02:26 +0000

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,8 @@
-import transformers
+from transformers import pipeline
 
-class CodeTokenizer(transformers.AutoTokenizer):
-    def __init__(self, PhiCo-D-Instruct):
-        super().__init__.from_pretrained(model_name)
+def generate_code_from_model(input_text):
+    """Generates code using the bigscience/T0_3B model."""
+    model_name = 'bigscience/T0_3B'  # Choose your model
+    generator = pipeline('text-generation', model=model_name)
+    code = generator(input_text, max_length=50, num_return_sequences=1, do_sample=True)[0]['generated_text']
+    return code
